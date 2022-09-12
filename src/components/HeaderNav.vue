c2bb7286e1e323cdff36c115b219a5e26158bc74
Farvon <farvon@gmail.com>
2022-09-11 23:51:14 -0300

    update

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -4,6 +4,9 @@
 
 <template>
     <div class="headerNav">
+        <div class="logoContainer">
+            <img class="logo" src="../assets/logo.svg" alt="Logo" />
+        </div>
     </div>
 </template>
 
@@ -13,5 +16,19 @@
         width: 100%;
         height: 60px;
         background-color: #1aa4a8;
+    }
+
+    .logoContainer{
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        width: 130px;
+        /*border: solid 1px red;*/
+    }
+
+    .logo{
+        display: flex;
+        width:60px;
+        margin-left: -10px;
     }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,14 +3,50 @@
 </script>
 
 <template>
-    <div class="sidebarContainer"></div>
+    <div class="sidebarContainer">
+
+        <form action="#" method="post">
+            <input class="inputBuscador" type="text" name="buscador" id="buscadorSideBar">
+
+            <button class="btnBuscador" type="submit">Buscar</button>
+        </form>
+
+
+    </div>
 </template>
 
 <style scoped>
 .sidebarContainer {
     display: flex;
-    width: 14vw;
+    justify-content: center;
+    width: 130px;
     height: 100vh;
     background-color: #212529;
 }
+/* 
+@media (max-width:768px){
+    .sidebarContainer{
+        display:none;
+    }
+} */
+
+.inputBuscador{
+    display: flex;
+    width:60%;
+    margin: auto;
+    margin-top: 20px;
+
+    font-size: smaller;
+}
+
+.btnBuscador{
+    display: flex;
+    width: 40%;
+    margin: auto;
+    margin-top: 10px;
+
+    font-size: 0.7em;
+}
+
+
 </style>
