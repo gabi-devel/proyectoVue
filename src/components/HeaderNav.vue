a5a8746bf699fc3a19e2308ef8ccec2e498db746
gabi-devel <gabrielabustos07@gmail.com>
2022-09-11 22:48:45 -0300

    proyectoVue

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    defineProps<{
+      msg: string
+    }>()
+</script>
+
+<template>
+    <div class="headerNav">
+    </div>
+</template>
+
+<style scoped>
+    .headerNav {
+        display: flex;
+        width: 100%;
+        height: 60px;
+        background-color: #1aa4a8;
+    }
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="sidebarContainer"></div>
+</template>
+
+<style scoped>
+.sidebarContainer {
+    display: flex;
+    width: 14vw;
+    height: 100vh;
+    background-color: #212529;
+}
+</style>
